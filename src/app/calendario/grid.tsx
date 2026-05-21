5de811070291e4852bad1a0d8d419c739a75f0f2
Luiz Felipe <luizfgs2006@gmail.com>
2026-05-20 23:18:08 -0300

    Finalizei a estrutura básica do calend.

--- src/app/calendario/grid.tsx
+++ src/app/calendario/grid.tsx
@@ -1,39 +1,27 @@
 import "./grid.css";
 
+function getDaysInCurrentMonth(): number{
+    const actualMonth = new Date().getMonth() + 1; // Obter o mês atual (0-11, então adicionamos 1)
+    const actualYear = new Date().getFullYear(); // Obter o ano atual
+    
+    return new Date(actualYear, actualMonth, 0).getDate();
+}
+
 export default function Grid(){
+    const daysInMonth = getDaysInCurrentMonth();
+
+    const days = [];
+    for (let i = 1; i <= daysInMonth; i++) {
+        days.push(
+        <div className="day">
+            <span className="n-day">{i}</span>
+            </div>
+    );
+    }
+
     return(
         <section className="calendar">
-            <div className="day">1</div>
-            <div className="day">2</div>
-            <div className="day">3</div>
-            <div className="day">4</div>
-            <div className="day">5</div>
-            <div className="day">6</div>
-            <div className="day">7</div>
-            <div className="day">8</div>
-            <div className="day">9</div>
-            <div className="day">10</div>
-            <div className="day">11</div>
-            <div className="day">12</div>
-            <div className="day">13</div>
-            <div className="day">14</div>
-            <div className="day">15</div>
-            <div className="day">16</div>
-            <div className="day">17</div>
-            <div className="day">18</div>
-            <div className="day">19</div>
-            <div className="day">20</div>
-            <div className="day">21</div>
-            <div className="day">22</div>
-            <div className="day">23</div>
-            <div className="day">24</div>
-            <div className="day">25</div>   
-            <div className="day">26</div>
-            <div className="day">27</div>
-            <div className="day">28</div>
-            <div className="day">29</div>
-            <div className="day">30</div>
-            <div className="day">31</div>
+            {days}
         </section>
     )
 }
